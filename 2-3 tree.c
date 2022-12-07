b7c0415176d05584a3d956f07ca2959ba90b4cae
Tauksik <84122841+Tauksik@users.noreply.github.com>
2022-12-07 12:42:39 +0530

    Update 2-3 tree.c

--- 2-3 tree.c
+++ 2-3 tree.c
@@ -1,119 +1,279 @@
-#include "2-3-tree.h"
-
-/********** 23tree_alloc **********/
-23tree *23tree_alloc() {
-  23tree *tree;
-  23tree_node *root;
-
-  tree = malloc(sizeof(23tree));
-  tree->n = 0;
-  root = tree->root = malloc(sizeof(23tree_node));
-  tree->stack = malloc(STACK_SIZE * sizeof(23tree_node *));
-  root->key1 = r->key2 = 0;
-  root->link_kind = LEAF;
-  root->left->node = root->middle->node = root->right->node = NULL;
-
-  return t;
-}
-
-/********** 23tree_free **********/
-void 23tree_free(23tree *tree) {
-  int tos;
-  23tree_node *p, *stack;
-
-  stack = malloc(2 * STACK_SIZE * sizeof(23tree_node *));
-  stack[0] = tree->root;
-  tos = 1;
-
-  while(tos) {
-    p = stack[--tos];
-    if(p->link_kind == INTERNAL) {
-      stack[tos++] = p->left.node;
-      stack[tos++] = p->middle.node;
-      if(p->right.node) stack[tos++] = p->right.node;
-  }x
-    free(p);
-  }
-
-  free(stack);
-
-  free(tree->stack);
-  free(tree);
-}
-
-/********** 23tree_insert **********/
-void *23tree_insert(23tree *tree, int item) {
-
-    23tree_node *temp = NULL;
-    23tree_node *temp2 = NULL;
-
-    if(tree->n == 0) {
-        tree->root->key1 = item;
-        tree->root->n++;
+#include <stdio.h>
+#include <stdlib.h>
+#define M 3
+struct node {
+    int n;
+    int keys[M-1];
+    struct node *p[M];
+}*root=NULL;
+enum KeyStatus { Duplicate,SearchFailure,Success,InsertIt,LessKeys };
+void insert(int key);
+void display(struct node *root,int);
+void DelNode(int x);
+enum KeyStatus ins(struct node *r, int x, int* y, struct node** u);
+int searchPos(int x,int *key_arr, int n);
+enum KeyStatus del(struct node *r, int x);
+void eatline(void);
+int main()
+{
+    int key;
+    int choice;
+    while(1)
+    {
+        printf("1.Insert\n");
+        printf("2.Delete\n");
+        printf("3.Display\n");
+        printf("4.exit\n");
+        printf("enter choice: ");
+        scanf("%d",&choice); eatline();
+        switch(choice)
+        {
+        case 1:
+            printf("enter value: ");
+            scanf("%d",&key); eatline();
+            insert(key);
+            break;
+        case 2:
+            printf("enter value: ");
+            scanf("%d",&key); eatline();
+            DelNode(key);
+            break;
+        case 3:
+            printf("2-3 tree:\n");
+            display(root,0);
+            break;
+        case 4:
+            exit(1);
+        default:
+            printf("choice is wrong\n");
+            break;
+        }
     }
-    else {
-        //only root node with one key
-        if(tree->n == 1) {
-            if(item > tree->root->key1) {
-                tree->root->key2 = tree->root->key1;
-                tree->root->key1 = item;
-            } else {
-                tree->root->key2 = item;
-            }
-        }
-
-        //only root node with two keys
-        else if(tree->n == 2) {
-            temp = malloc(sizeof(*23_node));
-            temp->key1 = temp->key2 = 0;
-            temp->left = temp->middle = temp->right = NULL;
-            temp->link_kind = LEAF;
-
-            temp2 = malloc(sizeof(*23_node));
-            temp2->key1 = temp2->key2 = 0;
-            temp2->left = temp2->middle = temp2->right = NULL;
-            temp->link_kind = LEAF;
-
-            if(item < tree->root->key1) {
-                temp->key1 = item;
-                temp2->key1 = tree->root->key2;
-                tree->root->key2 = 0;
-            } else if (item < tree->root->key2) {
-                temp->key1 = tree->root->key1;
-                temp2->key1 = tree->root->key2;
-                tree->root->key1 = item;
-                tree->root->key2 = 0;
-            } else {
-                temp->key1 = tree->root->key1;
-                temp2->key1 = item;
-                tree->root->key1 = tree->root->key2;
-                tree->root->key2 = 0;
-            }
-            tree->root->left = temp;
-            tree->root->right = temp2;
-        }
-
-        //tree has more than 2 keys
-        else {
-            if(item < tree->root->key1) {
-
-            } else if item < tree->root->key2) {
-
-            } else {
-
-            }
-        }
-
-        tree->n++;
+    return 0;
+}
+void insert(int key)
+{
+    struct node *newnode;
+    int upKey;
+    enum KeyStatus value;
+    value = ins(root, key, &upKey, &newnode);
+    if (value == Duplicate)
+        printf("number is already there\n");
+    if (value == InsertIt)
+    {
+        struct node *uproot = root;
+        root=malloc(sizeof(struct node));
+        root->n = 1;
+        root->keys[0] = upKey;
+        root->p[0] = uproot;
+        root->p[1] = newnode;
     }
 }
+enum KeyStatus ins(struct node *ptr, int key, int *upKey,struct node **newnode)
+{
+    struct node *newPtr, *lastPtr;
+    int pos, i, n,splitPos;
+    int newKey, lastKey;
+    enum KeyStatus value;
+    if (ptr == NULL)
+    {
+        *newnode = NULL;
+        *upKey = key;
+        return InsertIt;
+    }
+    n = ptr->n;
+    pos = searchPos(key, ptr->keys, n);
+    if (pos < n && key == ptr->keys[pos])
+        return Duplicate;
+    value = ins(ptr->p[pos], key, &newKey, &newPtr);
+    if (value != InsertIt)
+        return value;
+    if (n < M - 1)
+    {
+        pos = searchPos(newKey, ptr->keys, n);
+        for (i=n; i>pos; i--)
+        {
+            ptr->keys[i] = ptr->keys[i-1];
+            ptr->p[i+1] = ptr->p[i];
+        }
+        ptr->keys[pos] = newKey;
+        ptr->p[pos+1] = newPtr;
+        ++ptr->n;
+        return Success;
+    }
+    if (pos == M - 1)
+    {
+        lastKey = newKey;
+        lastPtr = newPtr;
+    }
+    else
+    {
+        lastKey = ptr->keys[M-2];
+        lastPtr = ptr->p[M-1];
+        for (i=M-2; i>pos; i--)
+        {
+            ptr->keys[i] = ptr->keys[i-1];
+            ptr->p[i+1] = ptr->p[i];
+        }
+        ptr->keys[pos] = newKey;
+        ptr->p[pos+1] = newPtr;
+    }
+    splitPos = (M - 1)/2;
+    (*upKey) = ptr->keys[splitPos];
 
-/********** 23tree_find **********/
-void *23tree_find(23tree *tree, int key_item) {
-
+    (*newnode)=malloc(sizeof(struct node));
+    ptr->n = splitPos;
+    (*newnode)->n = M-1-splitPos;
+    for (i=0; i < (*newnode)->n; i++)
+    {
+        (*newnode)->p[i] = ptr->p[i + splitPos + 1];
+        if(i < (*newnode)->n - 1)
+            (*newnode)->keys[i] = ptr->keys[i + splitPos + 1];
+        else
+            (*newnode)->keys[i] = lastKey;
+    }
+    (*newnode)->p[(*newnode)->n] = lastPtr;
+    return InsertIt;
 }
-
-/********** 23tree_find_min **********/
-void *23tree_find_min(23tree *tree) {
-  return t->min_item;
+void display(struct node *ptr, int blanks)
+{
+    if (ptr)
+    {
+        int i;
+        for(i=1; i<=blanks; i++)
+            printf(" ");
+        for (i=0; i < ptr->n; i++)
+            printf("%d ",ptr->keys[i]);
+        printf("\n");
+        for (i=0; i <= ptr->n; i++)
+            display(ptr->p[i], blanks+10);
+    }
+}
+int searchPos(int key, int *key_arr, int n)
+{
+    int pos=0;
+    while (pos < n && key > key_arr[pos])
+        pos++;
+    return pos;
+}
+void DelNode(int key)
+{
+    struct node *uproot;
+    enum KeyStatus value;
+    value = del(root,key);
+    switch (value)
+    {
+    case SearchFailure:
+        printf("number %d not found\n",key);
+        break;
+    case LessKeys:
+        uproot = root;
+        root = root->p[0];
+        free(uproot);
+        break;
+    }
+}
+enum KeyStatus del(struct node *ptr, int key)
+{
+    int pos, i, pivot, n ,min;
+    int *key_arr;
+    enum KeyStatus value;
+    struct node **p,*lptr,*rptr;
+    if (ptr == NULL)
+        return SearchFailure;
+    n=ptr->n;
+    key_arr = ptr->keys;
+    p = ptr->p;
+    min = (M - 1)/2;
+    pos = searchPos(key, key_arr, n);
+    if (p[0] == NULL)
+    {
+        if (pos == n || key < key_arr[pos])
+            return SearchFailure;
+        for (i=pos+1; i < n; i++)
+        {
+            key_arr[i-1] = key_arr[i];
+            p[i] = p[i+1];
+        }
+        return --ptr->n >= (ptr==root ? 1 : min) ? Success : LessKeys;
+    }
+    if (pos < n && key == key_arr[pos])
+    {
+        struct node *qp = p[pos], *qp1;
+        int nkey;
+        while(1)
+        {
+            nkey = qp->n;
+            qp1 = qp->p[nkey];
+            if (qp1 == NULL)
+                break;
+            qp = qp1;
+        }
+        key_arr[pos] = qp->keys[nkey-1];
+        qp->keys[nkey - 1] = key;
+    }
+    value = del(p[pos], key);
+    if (value != LessKeys)
+        return value;
+    if (pos > 0 && p[pos-1]->n > min)
+    {
+        pivot = pos - 1;
+        lptr = p[pivot];
+        rptr = p[pos];
+        rptr->p[rptr->n + 1] = rptr->p[rptr->n];
+        for (i=rptr->n; i>0; i--)
+        {
+            rptr->keys[i] = rptr->keys[i-1];
+            rptr->p[i] = rptr->p[i-1];
+        }
+        rptr->n++;
+        rptr->keys[0] = key_arr[pivot];
+        rptr->p[0] = lptr->p[lptr->n];
+        key_arr[pivot] = lptr->keys[--lptr->n];
+        return Success;
+    }
+    if (pos < n && p[pos + 1]->n > min)
+    {
+        pivot = pos;
+        lptr = p[pivot];
+        rptr = p[pivot+1];
+        lptr->keys[lptr->n] = key_arr[pivot];
+        lptr->p[lptr->n + 1] = rptr->p[0];
+        key_arr[pivot] = rptr->keys[0];
+        lptr->n++;
+        rptr->n--;
+        for (i=0; i < rptr->n; i++)
+        {
+            rptr->keys[i] = rptr->keys[i+1];
+            rptr->p[i] = rptr->p[i+1];
+        }
+        rptr->p[rptr->n] = rptr->p[rptr->n + 1];
+        return Success;
+    }
+    if(pos == n)
+        pivot = pos-1;
+    else
+        pivot = pos;
+    lptr = p[pivot];
+    rptr = p[pivot+1];
+    lptr->keys[lptr->n] = key_arr[pivot];
+    lptr->p[lptr->n + 1] = rptr->p[0];
+    for (i=0; i < rptr->n; i++)
+    {
+        lptr->keys[lptr->n + 1 + i] = rptr->keys[i];
+        lptr->p[lptr->n + 2 + i] = rptr->p[i+1];
+    }
+    lptr->n = lptr->n + rptr->n +1;
+    free(rptr);
+    for (i=pos+1; i < n; i++)
+    {
+        key_arr[i-1] = key_arr[i];
+        p[i] = p[i+1];
+    }
+    return --ptr->n >= (ptr == root ? 1 : min) ? Success : LessKeys;
+}
+void eatline(void) {
+  char c;
+  printf("");
+  while (c=getchar()!='\n') ;
 }
