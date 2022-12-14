bd5397c9dc1ac3a3ca2bec1cc89de9d11a28630d
Tauksik <84122841+Tauksik@users.noreply.github.com>
2022-12-14 11:58:48 +0530

    Update avl tree.c

--- avl tree.c
+++ avl tree.c
@@ -1,113 +1,281 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct Node
+struct node
 {
-	int key;
-	struct Node *left;
-	struct Node *right;
-	int height;
+    int data;
+    struct node* left;
+    struct node* right;
+    int ht;
 };
-int height(struct Node *N)
-{
-	if (N == NULL)
-		return 0;
-	return N->height;
-}
-int max(int a, int b)
-{
-	return (a > b)? a : b;
-}
-struct Node* newNode(int key)
-{
-	struct Node* node = (struct Node*)
-						malloc(sizeof(struct Node));
-	node->key = key;
-	node->left = NULL;
-	node->right = NULL;
-	node->height = 1; // new node is initially added at leaf
-	return(node);
-}
-struct Node *rightRotate(struct Node *y)
-{
-	struct Node *x = y->left;
-	struct Node *T2 = x->right;
-	x->right = y;
-	y->left = T2;
-	y->height = max(height(y->left),
-					height(y->right)) + 1;
-	x->height = max(height(x->left),
-					height(x->right)) + 1;
-	return x;
-}
-struct Node *leftRotate(struct Node *x)
-{
-	struct Node *y = x->right;
-	struct Node *T2 = y->left;
-	y->left = x;
-	x->right = T2;
-	x->height = max(height(x->left),
-					height(x->right)) + 1;
-	y->height = max(height(y->left),
-					height(y->right)) + 1;
-	return y;
-}
-int getBalance(struct Node *N)
-{
-	if (N == NULL)
-		return 0;
-	return height(N->left) - height(N->right);
-}
-struct Node* insert(struct Node* node, int key)
-{
-	if (node == NULL)
-		return(newNode(key));
-	if (key < node->key)
-		node->left = insert(node->left, key);
-	else if (key > node->key)
-		node->right = insert(node->right, key);
-	else // Equal keys are not allowed in BST
-		return node;
-	node->height = 1 + max(height(node->left),
-						height(node->right));
-		node to check whether this node became
-		unbalanced */
-	int balance = getBalance(node);
-	if (balance > 1 && key < node->left->key)
-		return rightRotate(node);
-	if (balance < -1 && key > node->right->key)
-		return leftRotate(node);
-	if (balance > 1 && key > node->left->key)
-	{
-		node->left = leftRotate(node->left);
-		return rightRotate(node);
-	}
-	if (balance < -1 && key < node->right->key)
-	{
-		node->right = rightRotate(node->right);
-		return leftRotate(node);
-	}
-	return node;
-}
-void preOrder(struct Node *root)
-{
-	if(root != NULL)
-	{
-		printf("%d ", root->key);
-		preOrder(root->left);
-		preOrder(root->right);
-	}
-}
+struct node* root = NULL;
+struct node* create(int);
+struct node* insert(struct node*, int);
+struct node* deleteN(struct node*, int);
+struct node* search(struct node*, int);
+struct node* rotate_left(struct node*);
+struct node* rotate_right(struct node*);
+int balance_factor(struct node*);
+int height(struct node*);
+void inorder(struct node*);
+void preorder(struct node*);
+void postorder(struct node*);
 int main()
 {
-struct Node *root = NULL;
-root = insert(root, 10);
-root = insert(root, 20);
-root = insert(root, 30);
-root = insert(root, 40);
-root = insert(root, 50);
-root = insert(root, 25);
-printf("Preorder traversal of the constructed AVL"
-		" tree is \n");
-preOrder(root);
-return 0;
+    int user_choice, data;
+    char user_continue = 'y';
+    struct node* result = NULL;
+    while (1)
+    {
+       printf("AVL TREE\n");
+        printf("\n1.Insert");
+        printf("\n2.Delete");
+        printf("\n3.Search");
+        printf("\n4.Inorder");
+        printf("\n5.Preorder");
+        printf("\n6.Postorder");
+        printf("\n7.EXIT");
+        printf("\n\nEnter Your Choice: ");
+        scanf("%d", &user_choice);
+        switch(user_choice)
+        {
+            case 1:
+                printf("\nEnter data: ");
+                scanf("%d", &data);
+                root = insert(root, data);
+                break;
+            case 2:
+                printf("\nEnter data: ");
+                scanf("%d", &data);
+                root = deleteN(root, data);
+                break;
+            case 4:
+                inorder(root);
+                break;
+            case 5:
+                preorder(root);
+                break;
+            case 6:
+                postorder(root);
+                break;
+            case 7:
+                printf("\n\tProgram Terminated\n");
+                return 1;
+            default:
+                printf("\n\tInvalid Choice\n");
+        }
+    }
+    return 0;
+}
+struct node* create(int data)
+{
+    struct node* new_node = (struct node*) malloc (sizeof(struct node));
+    if (new_node == NULL)
+    {
+        printf("\nMemory can't be allocated\n");
+        return NULL;
+    }
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+struct node* rotate_left(struct node* root)
+{
+    struct node* right_child = root->right;
+    root->right = right_child->left;
+    right_child->left = root;
+    root->ht = height(root);
+    right_child->ht = height(right_child);
+    return right_child;
+}
+struct node* rotate_right(struct node* root)
+{
+    struct node* left_child = root->left;
+    root->left = left_child->right;
+    left_child->right = root;
+    root->ht = height(root);
+    left_child->ht = height(left_child);
+    return left_child;
+}
+int balance_factor(struct node* root)
+{
+    int lh, rh;
+    if (root == NULL)
+        return 0;
+    if (root->left == NULL)
+        lh = 0;
+    else
+        lh = 1 + root->left->ht;
+    if (root->right == NULL)
+        rh = 0;
+    else
+        rh = 1 + root->right->ht;
+    return lh - rh;
+}
+int height(struct node* root)
+{
+    int lh, rh;
+    if (root == NULL)
+    {
+        return 0;
+    }
+    if (root->left == NULL)
+        lh = 0;
+    else
+        lh = 1 + root->left->ht;
+    if (root->right == NULL)
+        rh = 0;
+    else
+        rh = 1 + root->right->ht;
+
+    if (lh > rh)
+        return (lh);
+    return (rh);
+}
+struct node* insert(struct node* root, int data)
+{
+    if (root == NULL)
+    {
+        struct node* new_node = create(data);
+        if (new_node == NULL)
+        {
+            return NULL;
+        }
+        root = new_node;
+    }
+    else if (data > root->data)
+    {
+        root->right = insert(root->right, data);
+        if (balance_factor(root) == -2)
+        {
+            if (data > root->right->data)
+            {
+                root = rotate_left(root);
+            }
+            else
+            {
+                root->right = rotate_right(root->right);
+                root = rotate_left(root);
+            }
+        }
+    }
+    else
+    {
+        root->left = insert(root->left, data);
+        if (balance_factor(root) == 2)
+        {
+            if (data < root->left->data)
+            {
+                root = rotate_right(root);
+            }
+            else
+            {
+                root->left = rotate_left(root->left);
+                root = rotate_right(root);
+            }
+        }
+    }
+    root->ht = height(root);
+    return root;
+}
+struct node * deleteN(struct node *root, int x)
+{
+    struct node * temp = NULL;
+
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (x > root->data)
+    {
+        root->right = deleteN(root->right, x);
+        if (balance_factor(root) == 2)
+        {
+            if (balance_factor(root->left) >= 0)
+            {
+                root = rotate_right(root);
+            }
+            else
+            {
+                root->left = rotate_left(root->left);
+                root = rotate_right(root);
+            }
+        }
+    }
+    else if (x < root->data)
+    {
+        root->left = deleteN(root->left, x);
+        if (balance_factor(root) == -2)
+        {
+            if (balance_factor(root->right) <= 0)
+            {
+                root = rotate_left(root);
+            }
+            else
+            {
+                root->right = rotate_right(root->right);
+                root = rotate_left(root);
+            }
+        }
+    }
+    else
+    {
+        if (root->right != NULL)
+        {
+            temp = root->right;
+            while (temp->left != NULL)
+                temp = temp->left;
+            root->data = temp->data;
+            root->right = deleteN(root->right, temp->data);
+            if (balance_factor(root) == 2)
+            {
+                if (balance_factor(root->left) >= 0)
+                {
+                    root = rotate_right(root);
+                }
+                else
+                {
+                    root->left = rotate_left(root->left);
+                    root = rotate_right(root);
+                }
+            }
+        }
+        else
+        {
+            return (root->left);
+        }
+    }
+    root->ht = height(root);
+    return (root);
+}
+void inorder(struct node* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+void preorder(struct node* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    printf("%d ", root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(struct node* root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->data);
 }
